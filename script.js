9072d2650b17af75b20fc9bae586f01ff7cb9146
mrayan1127 <mrayan1127@gmail.com>
2025-07-01 09:37:29 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function playMusic() {
+    const music = document.getElementById("bgMusic");
+    music.play();
+    launchConfetti();
+}
+
+function launchConfetti() {
+    const duration = 5 * 1000;
+    const end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 5,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+        });
+        confetti({
+            particleCount: 5,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
+}
